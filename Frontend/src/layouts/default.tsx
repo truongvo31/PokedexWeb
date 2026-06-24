3e6191ebfda9221130ad162196a24e14afe7f2c5
Phong Nguyen <nguyenvuhoangphong@gmail.com>
2026-06-24 17:07:58 +0900

    Updated Layout

--- Frontend/src/layouts/default.tsx
+++ Frontend/src/layouts/default.tsx
@@ -1,22 +1,46 @@
-import { Divider } from '@fluentui/react-components';
-import { Link, Outlet } from 'react-router-dom';
+import { Hamburger } from '@fluentui/react-components';
+import { useEffect, useState } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import { Nav } from '../components/ui/nav';
+import { useBreakpoint } from '../stores/useBreakpoint';
+
+const AppTitle = 'Pokedex';
 
 export default function DefaultLayout() {
+  const { md } = useBreakpoint();
+  const [open, setOpen] = useState(md);
+  const location = useLocation();
+
+  useEffect(() => {
+    setOpen(md);
+  }, [md]);
+
+  useEffect(() => {
+    if (!md) {
+      setOpen(false);
+    }
+  }, [location.pathname]);
+
   return (
     <div>
-      <header className="h-(--header-height)">
+      <header className="h-(--header-height) bg-(--colorNeutralBackground4) border-b border-(--colorNeutralStroke2)">
         <div className="flex h-full items-center justify-between px-4">
-          <div id="layout_header_left" className="flex gap-4">
-            <Link to="/">Pokemons</Link>
-            <Link to="/types">Types</Link>
+          <div id="layout_header_left" className="flex gap-2 items-center">
+            <Hamburger onClick={() => setOpen((prev) => !prev)} />
+            {AppTitle}
           </div>
         </div>
-        <Divider />
       </header>
-      <Nav />
-      <main className="flex flex-col h-[calc(100dvh-var(--header-height))] overflow-auto">
-        <Outlet />
+      <main className="flex h-[calc(100dvh-var(--header-height))]">
+        <Nav
+          open={open}
+          overlay={!md}
+          headerText={AppTitle}
+          onToggle={async () => setOpen((prev) => !prev)}
+        />
+        <div className="h-full flex-1 overflow-auto rounded p-2">
+          <Outlet />
+        </div>
       </main>
     </div>
   );
---
+++ Frontend/src/stores/useBreakpoint.ts
@@ -0,0 +1,10 @@
+import { useMediaQuery } from './effects/useMediaQuery';
+
+export function useBreakpoint() {
+  return {
+    sm: useMediaQuery('(min-width: 640px)'),
+    md: useMediaQuery('(min-width: 768px)'),
+    lg: useMediaQuery('(min-width: 1024px)'),
+    xl: useMediaQuery('(min-width: 1280px)'),
+  };
+}
